b0300f0a8880fcb0a41884e5ab382eefb8d678d8
lealrenato <renatolealhiss@gmail.com>
2022-11-05 15:17:00 -0300

    add shared preferences inicializando atributos

--- app/src/main/java/com/guaraTecnologia/aulaappsp/MainActivity.java
+++ app/src/main/java/com/guaraTecnologia/aulaappsp/MainActivity.java
@@ -2,13 +2,43 @@ package com.guaraTecnologia.aulaappsp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "APP_SP";
+    private static final String PREF_NOME = "APP_SP_pref";
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor dados;
+
+    String nomeProduto;
+    int codigoProduto;
+    float precoProduto;
+    boolean estoque;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: Rodando ");
+
+        sharedPreferences = getSharedPreferences(PREF_NOME, Context.MODE_PRIVATE);
+
+        Log.i(TAG, "onCreate: Pasta shared criada");
+
+        dados = sharedPreferences.edit();
+        nomeProduto = "notebook";
+        codigoProduto = 12;
+        precoProduto = 2500.00f;
+        estoque = true;
     }
 }
